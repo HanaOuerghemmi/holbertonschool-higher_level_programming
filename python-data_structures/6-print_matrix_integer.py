d60af17ca5722c471dc3bccad03f729d9143ecd8
HanaOuerghemmi <5004@holbertonschool.com>
2022-09-13 14:26:25 +0100

    👹matrix👹

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,8 @@ def print_matrix_integer(matrix=[[]]):
         return None
     for row in matrix:
         for i in row:
+            if i == len(row) - 1:
+                 print("{:d}".format(i), end="")
+
             print("{:d}".format(i), end=" ")
         print("")
