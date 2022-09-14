801622e897ba48a7b3a304edbcdf7d8f76166465
Hana Ouerghemmi <105166222+HanaOuerghemmi@users.noreply.github.com>
2022-09-14 22:33:18 +0100

    Update 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,12 @@
 def roman_to_int(roman_string):
     if roman_string is None or not isinstance(roman_string, str):
         return 0
-    rvalue = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-            'C': 100, 'D': 500, 'M': 1000}
+    rval = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                 'C': 100, 'D': 500, 'M': 1000}
     int_value = 0
     for i in range(len(roman_string)):
-        if i > 0 and rvalue[roman_string[i]] > rvalue[roman_string[i - 1]]:
-            int_value += rvalue[roman_string[i]] - 2 * rvalue[roman_string[i - 1]]
+        if i > 0 and rval[roman_string[i]] > rval[roman_string[i - 1]]:
+            int_value += rval[roman_string[i]] - 2 * rval[roman_string[i - 1]]
         else:
-            int_value += rvalue[roman_string[i]]
+            int_value += rval[roman_string[i]]
     return int_value
