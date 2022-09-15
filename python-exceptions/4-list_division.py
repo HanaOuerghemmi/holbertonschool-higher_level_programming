96918718c3cc64b8e2f0bfdcd7a225471edd32d7
HanaOuerghemmi <5004@holbertonschool.com>
2022-09-15 10:05:32 +0100

    🤯div list error🤯

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -11,7 +11,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("division by 0")
             a = 0
         except IndexError:
-            print("out of the range")
+            print("out of range")
             a = 0
 
         finally:
