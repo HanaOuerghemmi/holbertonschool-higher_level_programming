4e941e054eb667dd164f2574b33e36b73584ff24
HanaOuerghemmi <5004@holbertonschool.com>
2022-09-27 11:50:45 +0100

    🙃bae geometreique / rectangle class😕

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,27 +1,7 @@
 #!/usr/bin/python3
 """ geometry module """
 
-
-class BaseGeometry():
-    """ Base gemetry class """
-
-    def area(self):
-        """
-        Public instance method that raises an Exception
-        """
-
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Public instance method:  hat validates value
-        """
-
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
+BaseGeometry = __import__(7-base_geometry.py).BaseGeometry
 
 class Rectangle(BaseGeometry):
     """
