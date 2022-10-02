c4d733d3907c530d4e7c8f3cf0ea3244ba56f38d
HanaOuerghemmi <5004@holbertonschool.com>
2022-10-02 19:47:19 +0100

    🤫read a file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,7 +2,7 @@
 """ read a file """
 
 def read_file(filename=""):
-    """ function tht reads a text file UTF8 """
+    """ function that reads a text file UTF8 """
 
     with open(filename, encoding="utf-8") as filename:
         print(filename.read(), end="")
