61a0e0217bfdbb212f7dfdefdae245ee1f5ee887
HanaOuerghemmi <5004@holbertonschool.com>
2022-10-03 11:46:00 +0100

    🤨load add  save file json😏

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,9 +3,10 @@
 
 import sys
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+    load_from_json_file = __import__(
+        '6-load_from_json_file.py').load_from_json_file
 
     filename = "add_item.json"
     arg = sys.argv[1:]
@@ -17,4 +18,4 @@ if __name__ =="__main__":
         my_list = []
 
     my_list.extend(arg)
-    save_to_jon_file(my_list, filename)
+    save_to_json_file(my_list, filename)
