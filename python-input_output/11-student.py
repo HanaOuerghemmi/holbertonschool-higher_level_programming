65be760812b9c2f11bd8db29e44b04b5093742fa
HanaOuerghemmi <5004@holbertonschool.com>
2022-10-04 19:34:59 +0100

    🤨class student😏

---
+++ python-input_output/11-student.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""python input output"""
+
+
+class Student:
+    """ class that defines a student """
+
+    """ instantiation """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+        """ method """
+
+    def to_json(self, attrs=None):
+        """
+        public method that returns the dictionary dscription
+        with simpe data structre
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            stds = {}
+            for attribute in attrs:
+                if hasattr(self, attribute):
+                    """ hasattr function, to check if an attribute exist """
+                    stds[attribute] = getattr(self, attribute)
+                    """
+                    getattr function returns the value of the
+                    specified attribute from the specified object.
+                    """
+            return stds
+
+    def reload_from_json(self, json):
+        """ 
+        public method that replaces all attributes of the Student instance
+        """
+
+        self.__dict__.update(json)
+        """
+        The update() method updates the current set, by 
+        adding items from another set (or any other iterable).
+        """
