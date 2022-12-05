56383c7244a30d4e7b97d9d09d5fbfd66614dd29
HanaOuerghemmi <5004@holbertonschool.com>
2022-12-05 18:24:20 +0100

    f else  js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-let l = process.argv.length;
-if (l < 3){
+const l = process.argv.length;
+if (l < 3) {
     console.log('No argument');
-} else if (l === 3){
+} else if (l === 3) {
     console.log('Argument found');
 } else {
     console.log('Arguments found');
 }
+
 
