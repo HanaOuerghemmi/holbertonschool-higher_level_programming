4d8a0aef6769e32d5f32421e1f296361855d542a
HanaOuerghemmi <5004@holbertonschool.com>
2022-12-05 19:08:37 +0100

    an integer

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const number = parseInt(process.argv[2]);
-if (String(number) === 'NaN'){
-    console.log('Not a number');
+if (String(number) === 'NaN') {
+  console.log('Not a number');
 } else {
-    console.log('My number: ' + number);
+  console.log('My number: ' + number);
 }
+
