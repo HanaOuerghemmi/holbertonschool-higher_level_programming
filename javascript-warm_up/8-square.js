623f65db4a4174428c3b9cab38af67dadd3d2828
HanaOuerghemmi <5004@holbertonschool.com>
2022-12-05 19:39:01 +0100

    😤semistandard 😤

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const n = parseInt(process.argv[2]);
 if (n) {
-  for (let i = 0; i < n; i++){
-      console.log('X'.repeat(n));
+  for (let i = 0; i < n; i++) {
+    console.log('X'.repeat(n));
   }
 } else {
   console.log('Missing size');
