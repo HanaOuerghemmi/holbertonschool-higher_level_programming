7c894458305676128e960be01f09a40f94a16a80
HanaOuerghemmi <5004@holbertonschool.com>
2022-12-05 19:27:32 +0100

    😤semistandard 😤

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const str = 'C is fun';
 let i = 0;
-const n  = parseInt(process.argv[2]);
+const n = parseInt(process.argv[2]);
 if (n) {
-  while ( i < n ){
+  while (i < n) {
     console.log(str);
     i++;
   }
