66df8d69241678809f859d5949a27f42861adc44
HanaOuerghemmi <5004@holbertonschool.com>
2022-12-05 19:57:51 +0100

    😤semistandard 😤

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 function factorial (x) {
-  if (isNaN(x)){
+  if (isNaN(x)) {
     return 1;
-  } else if  (x === 0) {
+  } else if (x === 0) {
     return 1;
   } else {
     return x * factorial(x - 1);
