1720f76609e901b756338efa1c777e9f2a05b18b
HanaOuerghemmi <5004@holbertonschool.com>
2022-12-06 13:26:54 +0100

    😤semistandard 😤

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const l = process.argv.length;
 if (l <= 3) {
-    console.log(0);
+  console.log(0);
 } else {
   const array = process.argv.map(Number)
     .slice(2, l).sort((a, b) => a - b);
-  len = array.length;
+  const len = array.length;
   console.log(array[len - 2]);
 }
