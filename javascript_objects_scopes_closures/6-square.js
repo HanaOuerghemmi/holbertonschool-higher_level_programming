6390daa7a322dc416a25de9fe738f0eac93a0b2f
HanaOuerghemmi <5004@holbertonschool.com>
2022-12-12 11:10:03 +0100

    class square

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -5,14 +5,14 @@ module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
-  
+
   charPrint (c) {
-    if ( c == undefined){
-        c = 'X'
-    } 
-    const size = this.height
-    for (let i = 0; i < size; i++){
-        console.log(c.repeat(size));
+    if (c === undefined) {
+      c = 'X';
+    }
+    const size = this.height;
+    for (let i = 0; i < size; i++) {
+      console.log(c.repeat(size));
     }
   }
 };
